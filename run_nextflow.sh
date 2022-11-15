d83d30e672daa2941fcd98dcd89a8bd5eff70d0d
xianggenti <xiang.li@gentibio.com>
2022-11-15 15:36:43 -0500

    update running script

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -1,8 +1,10 @@
 ## Default testing
-#nextflow run nf-core/rnaseq -profile test,docker --outdir ./results/ -N xiang.li@gentibio.com
-#nextflow run nf-core/rnaseq --input /home/xli/code_archive/github/rnaseq/assets/samplesheet_EngTreg.csv --outdir ./results/EngTreg/ --genome GRCh38 -profile docker
-#nextflow run nf-core/rnaseq --input /home/xli/code_archive/github/rnaseq/assets/samplesheet_EngTreg.csv \
---outdir ./results/EngTreg/ --genome GRCh38 -profile docker \
+nextflow run nf-core/rnaseq -profile test,docker --outdir ./results/ \
 --max_cpus 7 --max_memory 24.GB \
 --skip_qualimap --skip_markduplicates --skip_stringtie \
 -N xiang.li@gentibio.com
+--resume
+#nextflow run nf-core/rnaseq --input /home/xli/code_archive/github/rnaseq/assets/samplesheet_EngTreg.csv --outdir ./results/EngTreg/ --genome GRCh38 -profile docker
+#nextflow run nf-core/rnaseq --input /home/xli/code_archive/github/rnaseq/assets/samplesheet_EngTreg.csv \
+#--outdir ./results/EngTreg/ --genome GRCh38 -profile docker \
+
