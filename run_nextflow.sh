7a789cbe7a2cabd7709c26a161f3f58ecc5cd6fb
xianggenti <xiang.li@gentibio.com>
2022-11-15 12:29:59 -0500

    run_script

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -1,5 +1,5 @@
 ## Default testing
-nextflow run nf-core/rnaseq -profile test,docker --outdir ./results/ -N xiang.li@gentibio.com
+#nextflow run nf-core/rnaseq -profile test,docker --outdir ./results/ -N xiang.li@gentibio.com
 #nextflow run nf-core/rnaseq --input /home/xli/code_archive/github/rnaseq/assets/samplesheet_EngTreg.csv --outdir ./results/EngTreg/ --genome GRCh38 -profile docker
 #nextflow run nf-core/rnaseq --input /home/xli/code_archive/github/rnaseq/assets/samplesheet_EngTreg.csv \
 --outdir ./results/EngTreg/ --genome GRCh38 -profile docker \
