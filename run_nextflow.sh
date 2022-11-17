63b395d67419f2153938c1b925fe569eaf0b087f
xianggenti <xiang.li@gentibio.com>
2022-11-17 10:52:38 -0500

    From download to process

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -1,10 +1,22 @@
-## Default testing
-nextflow run nf-core/rnaseq -profile test,docker --outdir ./results/ \
---max_cpus 7 --max_memory 24.GB \
---skip_qualimap --skip_markduplicates --skip_stringtie \
--N xiang.li@gentibio.com
---resume
-#nextflow run nf-core/rnaseq --input /home/xli/code_archive/github/rnaseq/assets/samplesheet_EngTreg.csv --outdir ./results/EngTreg/ --genome GRCh38 -profile docker
-#nextflow run nf-core/rnaseq --input /home/xli/code_archive/github/rnaseq/assets/samplesheet_EngTreg.csv \
-#--outdir ./results/EngTreg/ --genome GRCh38 -profile docker \
+PATH_S3=$1  #s3://upload-only-gentibio/uploads/30-776133146/
+PATH_fastq=$2
 
+
+aws s3 sync ${PATH_S3} ${PATH_fastq}
+
+wget -L https://raw.githubusercontent.com/nf-core/rnaseq/master/bin/fastq_dir_to_samplesheet.py
+chmod 744 ./fastq_dir_to_samplesheet.py
+ 
+./fastq_dir_to_samplesheet.py ${PATH_fastq} ${PATH_fastq}/samplesheet_ST2_Treg.csv
+
+
+## Default testing
+#nextflow run nf-core/rnaseq -profile test,docker --outdir ./results/ \
+
+nextflow run nf-core/rnaseq --input ${PATH_fastq}/samplesheet_ST2_Treg.csv \
+--outdir ./results/ST2_Treg/ --genome GRCh38 -profile docker \
+--max_cpus 63 --max_memory 250.GB \
+--skip_qualimap --skip_markduplicates --skip_stringtie \
+-N xiang.li@gentibio.com \
+-resume
+#--skip_trimming \
