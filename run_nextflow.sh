e495d737db0e62e508c9f5bf4867cfcbeab33a62
xianggenti <xiang.li@gentibio.com>
2022-11-30 13:55:03 -0500

    add custom reference

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -2,7 +2,7 @@ PATH_S3=$1  #s3://upload-only-gentibio/uploads/30-776133146/
 PATH_fastq=$2
 
 
-aws s3 sync ${PATH_S3} ${PATH_fastq}
+aws s3 sync ${PATH_S3} ${PATH_fastq} --force-glacier-transfer #in case for restored files
 
 wget -L https://raw.githubusercontent.com/nf-core/rnaseq/master/bin/fastq_dir_to_samplesheet.py
 chmod 744 ./fastq_dir_to_samplesheet.py
@@ -14,9 +14,11 @@ chmod 744 ./fastq_dir_to_samplesheet.py
 #nextflow run nf-core/rnaseq -profile test,docker --outdir ./results/ \
 
 nextflow run nf-core/rnaseq --input ${PATH_fastq}/samplesheet_ST2_Treg.csv \
---outdir ./results/ST2_Treg/ --genome GRCh38 -profile docker \
---max_cpus 63 --max_memory 250.GB \
---skip_qualimap --skip_markduplicates --skip_stringtie \
+--outdir ./results/ST2_Treg/ -profile docker \
+--max_cpus 16 --max_memory 128.GB \
+--skip_fastqc --skip_qualimap --skip_markduplicates --skip_stringtie \
 -N xiang.li@gentibio.com \
+--fasta /home/ec2-user/Processed_Data/ST2_LENTI.fa \
+--gtf /home/ec2-user/Processed_Data/ST2_LENTI.gtf \
 -resume
 #--skip_trimming \
