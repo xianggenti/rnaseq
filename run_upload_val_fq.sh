b8c61e53ef3d585ed6309a0932f7cce0263e3947
xianggenti <xiang.li@gentibio.com>
2022-12-15 17:28:35 -0500

    convert run script as LF(Unix)

--- run_upload_val_fq.sh
+++ run_upload_val_fq.sh
@@ -1,3 +1,6 @@
 S3_object_path=$1
 xx=$(find -name "*_val_*.gz")
-for x in ${xx[*]}; do aws s3 cp --follow-symlinks ${x} ${S3_object_path} ; done
+for x in ${xx[*]}
+do
+aws s3 cp --follow-symlinks ${x} ${S3_object_path}
+done
