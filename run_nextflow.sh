f37b2d300c3dc6d2bcae55d593cbe1d22d94f5fa
xianggenti <xiang.li@gentibio.com>
2023-02-25 01:08:52 -0500

    Update run_nextflow with r5a.8xlarge and upload trimmed files

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -1,25 +1,29 @@
-PATH_S3=$1  #s3://upload-only-gentibio/uploads/30-776133146/
+PATH_S3=$1  #s3://upload-only-gentibio/uploads/30-776133146/  # reach to the folder contains FASTQ.gz
 PATH_fastq=$2
-
+Name_Sample_Sheet="samplesheet_GNTI_122_Treg.csv"
 
 aws s3 sync ${PATH_S3} ${PATH_fastq} --force-glacier-transfer #in case for restored files
 
 wget -L https://raw.githubusercontent.com/nf-core/rnaseq/master/bin/fastq_dir_to_samplesheet.py
 chmod 744 ./fastq_dir_to_samplesheet.py
- 
-./fastq_dir_to_samplesheet.py ${PATH_fastq} ${PATH_fastq}/samplesheet_ST2_Treg.csv
+
+
+
+echo "./fastq_dir_to_samplesheet.py ${PATH_fastq}/ ${PATH_fastq}/${Name_Sample_Sheet}"
+./fastq_dir_to_samplesheet.py ${PATH_fastq}/ ${PATH_fastq}/${Name_Sample_Sheet}
 
 
 ## Default testing
 #nextflow run nf-core/rnaseq -profile test,docker --outdir ./results/ \
 
-nextflow run nf-core/rnaseq --input ${PATH_fastq}/samplesheet.csv \
+nextflow run nf-core/rnaseq --input ${PATH_fastq}/${Name_Sample_Sheet} \
 --outdir ./results/samples/ -profile docker \
---max_cpus 16 --max_memory 128.GB \
+--max_cpus 31 --max_memory 250.GB \
 --skip_fastqc --skip_qualimap --skip_markduplicates --skip_stringtie \
 -N xiang.li@gentibio.com \
 --genome GRCh38 \ ## default genome
---fasta /home/ec2-user/Processed_Data/ST2_LENTI.fa \
---gtf /home/ec2-user/Processed_Data/ST2_LENTI.gtf \
 -resume
+#--fasta /home/ec2-user/Processed_Data/ST2_LENTI.fa \
+#--gtf /home/ec2-user/Processed_Data/ST2_LENTI.gtf \
+
 #--skip_trimming \
--- run_upload_val_fq.sh
+++ run_upload_val_fq.sh
@@ -1,4 +1,4 @@
-S3_object_path=$1
+S3_object_path=$1  ## S3 path must ends with /, as a folder
 xx=$(find -name "*_val_*.gz")
 for x in ${xx[*]}
 do
