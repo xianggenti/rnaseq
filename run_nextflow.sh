48f311319122428571008bba4ec2a4dffd952518
xianggenti <108030029+xianggenti@users.noreply.github.com>
2023-11-08 09:03:57 -0500

    Update run_nextflow.sh
    Update run script

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -8,10 +8,9 @@ wget -L https://raw.githubusercontent.com/nf-core/rnaseq/master/bin/fastq_dir_to
 chmod 744 ./fastq_dir_to_samplesheet.py
 
 
-
 echo "./fastq_dir_to_samplesheet.py ${PATH_fastq}/ ${PATH_fastq}/${Name_Sample_Sheet}"
-./fastq_dir_to_samplesheet.py ${PATH_fastq}/ ${PATH_fastq}/${Name_Sample_Sheet}
-
+./fastq_dir_to_samplesheet.py ${PATH_fastq}/ ${PATH_fastq}/${Name_Sample_Sheet} \
+#--read1_extension "_1_val_1.fq.gz" --read2_extension "_2_val_2.fq.gz"
 
 ## Default testing
 #nextflow run nf-core/rnaseq -profile test,docker --outdir ./results/ \
@@ -21,9 +20,10 @@ nextflow run nf-core/rnaseq --input ${PATH_fastq}/${Name_Sample_Sheet} \
 --max_cpus 31 --max_memory 250.GB \
 --skip_fastqc --skip_qualimap --skip_markduplicates --skip_stringtie \
 -N xiang.li@gentibio.com \
---genome GRCh38 \ ## default genome
--resume
+--genome GRCh38 \
+-resume #0a19b60d-d5fe-4a26-9e01-7a63d0a1d300 (##session ID from nextflow log)
+
+### OPtional following:
 #--fasta /home/ec2-user/Processed_Data/ST2_LENTI.fa \
 #--gtf /home/ec2-user/Processed_Data/ST2_LENTI.gtf \
-
 #--skip_trimming \
